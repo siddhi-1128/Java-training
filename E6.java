16973c97958dc576c67105f4dc35eb336138b3ef
siddhi-1128 <126164524+siddhi-1128@users.noreply.github.com>
2024-09-16 11:59:28 +0530

    Add files via upload

---
+++ E6.java
@@ -0,0 +1,7 @@
+class E6{
+	public static void main(String args[])
+	{
+		int a=5,b=5;
+		System.out.println("Addition is "+(a+b));
+	}
+}
---
+++ E8.java
@@ -0,0 +1,8 @@
+class E8{
+	public static void main(String args[])
+	{
+		long l=9876543321l;
+		System.out.println(l);
+			
+	}
+}
